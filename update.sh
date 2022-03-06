aad47652ff4700a16b7952d4b19fb8e9eb28b82f
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-03-06 20:17:53 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,22 @@
+clear
+echo -e "$Yellow                            processing please wait..>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait...>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait.....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait......>"
+sleep 2.0
+clear
+echo " "
+cd $HOME
+rm -rf Wifi_Brutoforce
+git clone
+cd Wifi_Brutoforce
+bash start.sh
