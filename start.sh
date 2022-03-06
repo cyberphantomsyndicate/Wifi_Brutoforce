2cedd4816e5a81e5761daff92c04e569202595d2
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-03-06 20:17:36 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo -e "\e[1;31m"
+figlet Love u Jaan
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border QADIR
+
+echo " "
+pip install pywifi
+pip install colorama
+cd Wifi_Brutoforce
+python3 wifi.py
+                                   
